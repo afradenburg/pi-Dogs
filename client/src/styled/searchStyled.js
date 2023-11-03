6f30e5aa73bede4a1d14e084247fe00d65c189dd
afradenburg <andres.burg@hotmail.com>
2023-11-03 15:33:27 -0500

    responsive

--- client/src/styled/searchStyled.js
+++ client/src/styled/searchStyled.js
@@ -6,11 +6,15 @@ export const SearchStyled = styled.div`
   display: flex;
   flex-direction: row;
   text-align: center;
-  width: 105%;
+  width: 100%;
   align-items: center;
   align-self: center;
   justify-content: center;
   border: 5px;
   border-radius: 25px;
   gap: 3px;
+  
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
