9c57495c4295e962da541445b99aa44451adbbc2
afradenburg <andres.burg@hotmail.com>
2023-11-03 21:12:50 -0500

    devolviendo cambios

--- api/src/db.js
+++ api/src/db.js
@@ -9,7 +9,6 @@ const {
 const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  ssl: true,
 });
 const basename = path.basename(__filename);
 
