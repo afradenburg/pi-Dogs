348097ad4cedfa2f14c29c6f0fdc75c26ad9d5d3
afradenburg <andres.burg@hotmail.com>
2024-02-22 10:00:50 -0500

    update searchbar style

--- client/src/styled/containerFilter.js
+++ client/src/styled/containerFilter.js
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 export const Container = styled.button`
   background-color: #3498db;
   color: white;
+  margin-right: 15px;
   @media screen and (max-width: 768px) {
       padding: 10px 20px;
       border: none;
       border-radius: 5px;
       cursor: pointer;
+      margin-top: 15px;
   /* Estilos para pantallas más pequeñas */
  
 }
