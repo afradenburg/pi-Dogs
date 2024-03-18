2e974e0ace14ac97460ad2c32aedf28c3a6c5d5b
afradenburg <andres.burg@hotmail.com>
2024-03-18 13:48:57 -0500

    update landin page margin

--- client/src/views/landinPage.jsx
+++ client/src/views/landinPage.jsx
@@ -5,13 +5,13 @@ import { Title, Button } from '../styled/landinPageStyled';
 
 export const LandinPage = () => {
   return (
-    <Container style={{marginTop: "150px"}}>
+    <Container style={{marginTop: "40px"}}>
       <Title>INFO-DOGS</Title>
       <p>Bienvenido a nuestra página dedicada a los perros. Aquí encontrarás información interesante, imágenes adorables y podras crear a tus mascotas peludas.</p>
       <Link to={"/home"}>
         <Button>Ingresar</Button>
       </Link>
-      <img style={{width: "100%", marginTop: "20px"}} src="https://www.telemundo.com/sites/nbcutelemundo/files/images/gallery/2017/07/05/perro-de-raza-samoyedo-sobre-el-cesped-3.jpg"/>
+      <img style={{width: "100%", marginTop: "20px", borderRadius: "50px"}} src="https://www.telemundo.com/sites/nbcutelemundo/files/images/gallery/2017/07/05/perro-de-raza-samoyedo-sobre-el-cesped-3.jpg"/>
     </Container>
   );
 };
